8bc79f2ecf15ac5ce0cb612cb8bc76827c9594bf
pmtejas <thejas.chintu@gmail.com>
2025-07-09 11:10:41 +0530

    DQL,DDL,DML PERFECT

--- sql/01_Query_Data_SELECT.sql
+++ sql/01_Query_Data_SELECT.sql
@@ -21,7 +21,7 @@
 /* ==============================================================================
    COMMENTS
 =============================================================================== */
-
+USE MYDATABASE;
 -- This is a single-line comment.
 
 /* This
@@ -35,30 +35,44 @@
 =============================================================================== */
 
 -- Retrieve All Customer Data
-
+SELECT * 
+FROM customers ;
 
 -- Retrieve All Order Data
-
+SELECT * 
+FROM orders ;
 
 /* ==============================================================================
    SELECT FEW COLUMNS
 =============================================================================== */
 
 -- Retrieve each customer's name, country, and score.
-
+SELECT 
+first_name,
+country,
+score
+FROM customers ;
 
 /* ==============================================================================
    WHERE
 =============================================================================== */
 
 -- Retrieve customers with a score not equal to 0
-
+SELECT * 
+FROM customers 
+WHERE score!=0
 
 -- Retrieve customers from Germany
-
+SELECT * 
+FROM customers 
+WHERE country='Germany'
 
 -- Retrieve the name and country of customers from Germany
-
+SELECT 
+first_name,
+country
+FROM customers 
+WHERE country='Germany'
 
 /* ==============================================================================
    ORDER BY
@@ -66,38 +80,66 @@
 
 /* Retrieve all customers and 
    sort the results by the highest score first. */
-
+SELECT * 
+FROM customers 
+ORDER BY score DESC
 
 /* Retrieve all customers and 
    sort the results by the lowest score first. */
-
+SELECT * 
+FROM customers 
+ORDER BY score
 
 /* Retrieve all customers and 
    sort the results by the country. */
-
+SELECT * 
+FROM customers 
+ORDER BY country
 
 /* Retrieve all customers and 
    sort the results by the country and then by the highest score. */
-
+SELECT * 
+FROM customers 
+ORDER BY country,score DESC
 
 /* Retrieve the name, country, and score of customers 
    whose score is not equal to 0
    and sort the results by the highest score first. */
-
+SELECT 
+first_name,
+country,
+score 
+FROM customers 
+WHERE score!=0
+ORDER BY score DESC
 
 /* ==============================================================================
    GROUP BY
 =============================================================================== */
 
 -- Find the total score for each country
-
+SELECT 
+country,
+SUM(score) AS total_score
+FROM customers 
+GROUP BY country
 
 /* This will not work because 'first_name' is neither part of the GROUP BY 
    nor wrapped in an aggregate function. SQL doesn't know how to handle this column. */
-
+SELECT 
+country,
+first_name,
+SUM(score) as total_score
+FROM customers
+GROUP BY country
 
 -- Find the total score and total number of customers for each country
-
+SELECT 
+country,
+SUM(score) AS total_score,
+COUNT(*) AS total_customers
+FROM customers 
+GROUP BY country
 
 /* ==============================================================================
    HAVING
@@ -105,35 +147,53 @@
 
 /* Find the average score for each country
    and return only those countries with an average score greater than 430 */
-
+SELECT 
+country,
+AVG(score) AS average_score
+FROM customers 
+GROUP BY country
+HAVING AVG(score)>430
 
 /* Find the average score for each country
    considering only customers with a score not equal to 0
    and return only those countries with an average score greater than 430 */
-
+SELECT 
+country,
+AVG(score) AS average_Score
+FROM customers 
+WHERE score!=0
+GROUP BY country
+HAVING AVG(score)>430
 
 /* ==============================================================================
    DISTINCT
 =============================================================================== */
 
 -- Return Unique list of all countries
-
+SELECT distinct * from customers ;
 
 /* ==============================================================================
    TOP
 =============================================================================== */
 
 -- Retrieve only 3 Customers
-
+SELECT TOP 3 *
+FROM customers ;
 
 -- Retrieve the Top 3 Customers with the Highest Scores
-
+SELECT TOP 3 *
+FROM customers 
+ORDER BY score DESC 
 
 -- Retrieve the Lowest 2 Customers based on the score
-
+SELECT TOP 2 *
+FROM customers 
+ORDER BY score 
 
 -- Get the Two Most Recent Orders
-
+SELECT top 2 *
+FROM orders 
+ORDER BY order_date DESC
 
 /* ==============================================================================
    All Together
@@ -143,17 +203,35 @@
    considering only customers with a score not equal to 0
    and return only those countries with an average score greater than 430
    and sort the results by the highest average score first. */
-
+SELECT 
+country,
+AVG(score) as Average_Score
+FROM customers 
+WHERE score!=0
+GROUP BY country
+HAVING AVG(score)>430
+ORDER by AVG(score) desc
 
 /* ============================================================================== 
    COOL STUFF - Additional SQL Features
 =============================================================================== */
 
 -- Execute multiple queries at once
-
+SELECT * 
+FROM customers ;
+SELECT * 
+FROM orders ;
 
 /* Selecting Static Data */
 -- Select a static or constant value without accessing any table
-
+SELECT 123 as staticNumber ;
+SELECT 'hello' as staticString ;
 
 -- Assign a constant value to a column in a query
+SELECT 
+id,
+first_name,
+score,
+country,
+'New Customer' AS customerType
+FROM customers ;
--- sql/03_Data_Manipulation_DML.sql
+++ sql/03_Data_Manipulation_DML.sql
@@ -10,61 +10,85 @@
      3. DELETE - Removing Data from Tables
 =================================================================================
 */
-
+use mydatabase
 /* ============================================================================== 
    INSERT
 =============================================================================== */
 /* #1 Method: Manual INSERT using VALUES */
 -- Insert new records into the customers table
-
+insert into customers(id,first_name,country,score)
+VALUES (6,'nitya','india',540),
+       (7,'zuker','usa',800)
 
 -- Incorrect column order 
-
+insert into customers(id,first_name,country,score)
+VALUES ('zara',8,'uk',700)
     
 -- Incorrect data type in values
-
+insert into customers(id,first_name,country,score)
+VALUES ('zara',8,'uk',700)
 
 -- Insert a new record with full column values
-
+insert into customers(id,first_name,country,score)
+VALUES (8,'zara','uk',700)
 
 -- Insert a new record without specifying column names (not recommended)
-
+INSERT INTO customers 
+VALUES(9,'manoj','india',670) ;
     
 -- Insert a record with only id and first_name (other columns will be NULL or default values)
-
+INSERT INTO customers (id,first_name)
+VALUES(10,'zabi') ;
 
 /* #2 Method: INSERT DATA USING SELECT - Moving Data From One Table to Another */
 -- Copy data from the 'customers' table into 'persons'
-
+INSERT INTO persons(id,person_name,birth_date,phone) 
+SELECT 
+   id,
+   first_name,
+   null,
+   'Unknown'
+FROM customers ;
 
 /* ============================================================================== 
    UPDATE
 =============================================================================== */
 
 -- Change the score of customer with ID 6 to 0
-
+UPDATE customers 
+SET score = 0
+WHERE id=6 ;
 
 -- Change the score of customer with ID 10 to 0 and update the country to 'UK'
-
+update customers
+SET score=750
+WHERE id=3
 
 -- Update all customers with a NULL score by setting their score to 0
-
+UPDATE customers 
+SET score = 0
+WHERE score is NULL ;
 
 -- Verify the update
-
+SELECT * 
+FROM customers ;
 
 /* ============================================================================== 
    DELETE
 =============================================================================== */
 
 -- Select customers with an ID greater than 5 before deleting
-
+SELECT * 
+FROM customers 
+WHERE id>5
 
 -- Delete all customers with an ID greater than 5
-
+DELETE FROM customers
+WHERE id>5 ;
 
 -- Delete all data from the persons table
+DELETE FROM persons
 
 
 -- Faster method to delete all rows, especially useful for large tables
-
+TRUNCATE TABLE PERSONS
